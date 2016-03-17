adbdaf69b818fa3c24479b895c4d0ae3fdd74ca3
root <root@test-node-4.scc.kit.edu>
2016-03-17 13:31:58 +0100

    fix style for db.pp

--- modules/puppet-b2safe/manifests/db.pp
+++ modules/puppet-b2safe/manifests/db.pp
@@ -5,25 +5,14 @@
 #
 #
 
-  class db(
+  class puppet-b2safe::db(
 ){
 $os=hiera('puppet-b2safe::packages::os')
 
-  case $os{ 
-  'sl6.6':{
-  class {'puppet-b2safe::postgresql':}
+  case $os{
+  'sl6.6':      { class {'puppet-b2safe::postgresql':} }
+  'CentOS7':    {class {'puppet-b2safe::postgresql':}}
+  'Scientific7':{class {'puppet-b2safe::postgresql':}}
+  default:      {notify {'in default: nothing to do':}}
   }
-  'CentOS7':{
-  class {'puppet-b2safe::postgresql':}
-  }
-   'Scientific7':{
-  class {'puppet-b2safe::postgresql':}
-  }     
-  default: {
-  notify{"in default: nothing to do":}
-  }      
-	
- }
 }
- 
-
