723b6ed6c80e090fcc1acce70cc9399db8e8d3be
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-03-21 15:55:54 +0100

    change fix home directory path into a variable

--- manifests/b2safe.pp
+++ manifests/b2safe.pp
@@ -15,23 +15,23 @@ class b2safe::b2safe(
 
   file{ '/etc/rpm/macros':
     ensure  => 'present',
-    content => '%_topdir /home/irods/rpmbuild',
+    content => "%_topdir /home/${::b2safe::irods::account_name}/rpmbuild",
     mode    => '0644'
   }
 
   #Clone b2safe version 3.0.2 (work around)
   exec{ 'get_B2SAFE_rpm':
     path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
-    command => 'git clone https://github.com/EUDAT-B2SAFE/B2SAFE-core /home/irods/B2SAFE-core && cd /home/irods/B2SAFE-core && git reset --hard 6e30b3c32051710e2630aa7255739f28828940f8',
-    creates => '/home/irods/B2SAFE-core',
-    user    => 'irods'
+    command => "git clone https://github.com/EUDAT-B2SAFE/B2SAFE-core /home/${::b2safe::irods::account_name}/B2SAFE-core && cd /home/${::b2safe::irods::account_name}/B2SAFE-core && git reset --hard 6e30b3c32051710e2630aa7255739f28828940f8",
+    creates => "/home/${::b2safe::irods::account_name}/B2SAFE-core",
+    user    => $::b2safe::irods::account_name,
   } ->
 
   exec{ 'create_rpm':
     path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
-    cwd     => '/home/irods/B2SAFE-core/packaging',
+    cwd     => "/home/${::b2safe::irods::account_name}/B2SAFE-core/packaging",
     command => 'sh create_rpm_package.sh',
-    user    => 'irods'
+    user    => $::b2safe::irods::account_name,
   } ->
 
     #=======================================================
@@ -41,7 +41,7 @@ class b2safe::b2safe(
   exec{ 'install_rpm':
     unless  => 'rpm -qa |grep irods-eudat-b2safe',
     path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
-    command => 'rpm -ivh /home/irods/rpmbuild/RPMS/noarch/irods-eudat-b2safe-3.0-2.noarch.rpm',
+    command => "rpm -ivh /home/${::b2safe::irods::account_name}/rpmbuild/RPMS/noarch/irods-eudat-b2safe-3.0-2.noarch.rpm",
   } ->
 
   file { '/opt/eudat/b2safe/packaging/install.conf':
