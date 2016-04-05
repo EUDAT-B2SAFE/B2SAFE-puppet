e46e24d3f8e9d45e071a0c3a7a901f0ef19fb395
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-04-05 15:38:07 +0200

    reorganize packages part and make epel selectable

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,6 +2,8 @@
 # setup source files for required repositories depending on operation system
 
 class b2safe::packages(
+  $install_epel = true,
+  $dependencies = ['fuse-libs','perl','perl-JSON','python-jsonschema','python-psutil','python-requests','authd']
 ){
   notify{ "Operating system ${::operatingsystem}": }
   case $::operatingsystem{
@@ -9,10 +11,12 @@ class b2safe::packages(
       notify{ "Repos for ${::operatingsystem} ${operatingsystemrelease}": }
       case $::operatingsystemrelease {
         6.6: {
-          package { 'epel-release-6-8':
-            provider => rpm,
-            ensure   => installed,
-            source   => 'http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
+          if $::b2sade::package::install_epel{
+            package { 'epel-release-6-8':
+              provider => rpm,
+              ensure   => installed,
+              source   => 'http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
+            }
           }
 
           package { 'pgdg-sl93-9.3-2':
@@ -20,36 +24,34 @@ class b2safe::packages(
             ensure   => installed,
             source   => 'http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-sl93-9.3-2.noarch.rpm',
           }
-
-          ensure_packages(['fuse-libs','perl','perl-JSON','python-jsonschema','python-psutil','python-requests','authd'])
 
           package { 'irods-icat-4.1.5':
             provider => rpm,
             ensure   => installed,
             source   => 'ftp://ftp.renci.org/pub/irods/releases/4.1.5/centos6/irods-icat-4.1.5-centos6-x86_64.rpm',
-            require  => Package['fuse-libs','perl','perl-JSON','python-jsonschema','python-psutil','python-requests']
+            require  => Package[$::b2sade::package::dependencies]
           }
         }
         7.0, 7.1: {
-          package { 'epel-release-7-5':
-            provider => rpm,
-            ensure   => installed,
-            source   => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
-          }->
+          if $::b2sade::package::install_epel{
+            package { 'epel-release-7-5':
+              provider => rpm,
+              ensure   => installed,
+              source   => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
+            }
+          }
 
           package { 'pgdg-sl93-9.3-2':
             provider => rpm,
             ensure   => installed,
             source   => 'http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-sl93-9.3-2.noarch.rpm',
-          }->
-
-          class { 'install_packages': }->
+          }
 
           package { 'irods-icat-4.1.7':
             provider => rpm,
             ensure   => installed,
             source   => 'ftp://ftp.renci.org/pub/irods/releases/4.1.7/centos7/irods-icat-4.1.7-centos7-x86_64.rpm',
-            require  => Package['fuse-libs','perl','perl-JSON','python-jsonschema','python-psutil','python-requests']
+            require  => Package[$::b2sade::package::dependencies]
           }
         }
         default:
@@ -61,34 +63,33 @@ class b2safe::packages(
     'CentOS': {
       notify{ "Repos for ${::operatingsystem}": }
 
-      package { 'epel-release-7-5':
-        provider => rpm,
-        ensure   => installed,
-        source   => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
-      }->
+      if $::b2sade::package::install_epel{
+        package { 'epel-release-7-5':
+          provider => rpm,
+          ensure   => installed,
+          source   => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
+        }
+      }
 
       package { 'pgdg-centos93-9.3-2':
         provider => rpm,
         ensure   => installed,
         source   => 'http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-2.noarch.rpm',
-      }->
-
-      class { 'install_packages': }->
-
+      }
 
       package { 'irods-icat-4.1.6':
         provider => rpm,
         ensure   => installed,
         source   => 'ftp://ftp.renci.org/pub/irods/releases/4.1.6/centos7/irods-icat-4.1.6-centos7-x86_64.rpm',
-        require  => Package['fuse-libs','perl','perl-JSON','python-jsonschema','python-psutil','python-requests']
+        require  => Package[$::b2sade::package::dependencies]
       }
     }
     default: {
       notify{ "in default: nothing to do": }
     }
   }
-}
 
-class install_packages{
-  ensure_packages(['lsof','fuse-libs','perl','perl-JSON','python-psutil','python-jsonschema','python-requests','authd'])
+  package { $::b2sade::package::dependencies :
+    ensure  => installed,
+  }
 }
