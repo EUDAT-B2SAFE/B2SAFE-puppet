7117c440f7f9bdb72cc94b8ecc6c79b23063b85d
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-04-12 14:52:59 +0200

    syntax fixes in packeges class

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -13,26 +13,26 @@ class b2safe::packages(
 
   case $::operatingsystem{
     'Scientific': {
-      notify{ "Repos for ${::operatingsystem} ${operatingsystemrelease}": }
+      notify{ "Repos for ${::operatingsystem} ${::operatingsystemrelease}": }
       case $::operatingsystemrelease {
         6.6: {
           if $install_epel{
             package { 'epel-release-6-8':
-              provider => rpm,
               ensure   => installed,
+              provider => rpm,
               source   => 'http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
             }
           }
 
           package { 'pgdg-sl93-9.3-2':
-            provider => rpm,
             ensure   => installed,
+            provider => rpm,
             source   => 'http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-sl93-9.3-2.noarch.rpm',
           }
 
           package { 'irods-icat-4.1.5':
-            provider => rpm,
             ensure   => installed,
+            provider => rpm,
             source   => 'ftp://ftp.renci.org/pub/irods/releases/4.1.5/centos6/irods-icat-4.1.5-centos6-x86_64.rpm',
             require  => Package[$dependencies]
           }
@@ -40,21 +40,21 @@ class b2safe::packages(
         7.0, 7.1: {
           if $install_epel{
             package { 'epel-release-7-5':
-              provider => rpm,
               ensure   => installed,
+              provider => rpm,
               source   => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
             }
           }
 
           package { 'pgdg-sl93-9.3-2':
-            provider => rpm,
             ensure   => installed,
+            provider => rpm,
             source   => 'http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-sl93-9.3-2.noarch.rpm',
           }
 
           package { 'irods-icat-4.1.7':
-            provider => rpm,
             ensure   => installed,
+            provider => rpm,
             source   => 'ftp://ftp.renci.org/pub/irods/releases/4.1.7/centos7/irods-icat-4.1.7-centos7-x86_64.rpm',
             require  => Package[$dependencies]
           }
@@ -70,27 +70,27 @@ class b2safe::packages(
 
       if $install_epel{
         package { 'epel-release-7-5':
-          provider => rpm,
           ensure   => installed,
+          provider => rpm,
           source   => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
         }
       }
 
       package { 'pgdg-centos93-9.3-2':
-        provider => rpm,
         ensure   => installed,
+        provider => rpm,
         source   => 'http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-2.noarch.rpm',
       }
 
       package { 'irods-icat-4.1.6':
-        provider => rpm,
         ensure   => installed,
+        provider => rpm,
         source   => 'ftp://ftp.renci.org/pub/irods/releases/4.1.6/centos7/irods-icat-4.1.6-centos7-x86_64.rpm',
         require  => Package[$dependencies]
       }
     }
     default: {
-      notify{ "in default: nothing to do": }
+      notify{ 'in default: nothing to do': }
     }
   }
 
