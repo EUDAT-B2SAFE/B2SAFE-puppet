85ec823c845526e6b1e9e117c4d86848e0038cbc
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-04-12 10:59:55 +0200

    replace stage with ordering arrow operator

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,19 +9,12 @@ class b2safe(
 {
 
   class { 'b2safe::packages':
-    stage => 'pre',
-  }
-
+  } ->
   class{ 'db':
-  }
-
+  } ->
   class{ 'b2safe::irods':
-    stage =>'main'
-  }
-
+  } ->
   class{ 'b2safe::b2safe':
-    stage => 'post'
   }
-
 
 }
