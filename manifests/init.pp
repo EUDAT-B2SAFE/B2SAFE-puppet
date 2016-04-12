1e9c2018c8d3e2f4bb9c240dcf6719444ea3fa23
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-04-12 15:14:19 +0200

    try absolute class imports

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,13 @@ class b2safe(
 )
 {
 
-  class { 'b2safe::packages':
+  class { '::b2safe::packages':
   } ->
-  class{ 'db':
+  class{ '::b2safe::db':
   } ->
-  class{ 'b2safe::irods':
+  class{ '::b2safe::irods':
   } ->
-  class{ 'b2safe::b2safe':
+  class{ '::b2safe::b2safe':
   }
 
 }
