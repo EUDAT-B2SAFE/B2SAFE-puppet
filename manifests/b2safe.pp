4b9d74c9e6be5c913137b1c0d0d963dc5f39c11d
Sander Apweiler <sanderapweiler@gmail.com>
2016-04-19 14:43:38 +0200

    use case statement for OS specific stuff at B2SAFE class

--- manifests/b2safe.pp
+++ manifests/b2safe.pp
@@ -10,55 +10,58 @@ class b2safe::b2safe(
 ){
   $b2safe_package_version = regsubst($b2safe_version,'^(\d+)\.(\d+)\.(\d+)$','\1.\2-\3')
 
-  package{ 'rpm-build':
-    ensure   => 'installed',
-    provider => 'yum'
+  case $::operatingsystem{
+    'Scientific', 'CentOS': {
+      package{ 'rpm-build':
+        ensure   => 'installed',
+      } ->
 
-  } ->
+      file{ '/etc/rpm/macros':
+        ensure  => 'present',
+        content => "%_topdir /home/${::b2safe::irods::account_name}/rpmbuild",
+        mode    => '0644'
+      } ->
 
-  file{ '/etc/rpm/macros':
-    ensure  => 'present',
-    content => "%_topdir /home/${::b2safe::irods::account_name}/rpmbuild",
-    mode    => '0644'
-  }
+      exec{ 'create_rpm':
+        path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
+        cwd     => "/home/${::b2safe::irods::account_name}/B2SAFE-core/packaging",
+        onlyif  => "test ! -e /home/${::b2safe::irods::account_name}/rpmbuild/RPMS/noarch/irods-eudat-b2safe-${b2safe_package_version}.noarch.rpm",
+        command => 'sh create_rpm_package.sh',
+        user    => $::b2safe::irods::account_name,
+        require => Exec[ 'get_B2SAFE' ],
+      } ->
+
+        #=======================================================
+        # Install package and update configuration file
+        #======================================================
+
+      exec{ 'install_rpm':
+        unless  => 'rpm -qa |grep irods-eudat-b2safe',
+        path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
+        command => "rpm -ivh /home/${::b2safe::irods::account_name}/rpmbuild/RPMS/noarch/irods-eudat-b2safe-${b2safe_package_version}.noarch.rpm",
+      } ->
+
+      file { '/opt/eudat/b2safe/packaging/install.conf':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        content => template('b2safe/install.erb'),
+      }
+    }
+    default: {}
 
   #Clone b2safe version 3.0.2 (work around)
   package{ 'git':
     ensure => installed,
   }
 
-  exec{ 'get_B2SAFE_rpm':
+  exec{ 'get_B2SAFE':
     path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
     command => "git clone https://github.com/EUDAT-B2SAFE/B2SAFE-core /home/${::b2safe::irods::account_name}/B2SAFE-core && cd /home/${::b2safe::irods::account_name}/B2SAFE-core && git reset --hard v${::b2safe::b2safe::b2safe_version}",
     creates => "/home/${::b2safe::irods::account_name}/B2SAFE-core",
     user    => $::b2safe::irods::account_name,
     require => Package[ 'git' ],
-  } ->
-
-  exec{ 'create_rpm':
-    path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
-    cwd     => "/home/${::b2safe::irods::account_name}/B2SAFE-core/packaging",
-    onlyif  => "test ! -e /home/${::b2safe::irods::account_name}/rpmbuild/RPMS/noarch/irods-eudat-b2safe-${b2safe_package_version}.noarch.rpm",
-    command => 'sh create_rpm_package.sh',
-    user    => $::b2safe::irods::account_name,
-  } ->
-
-    #=======================================================
-    # Install package and update configuration file
-    #======================================================
-
-  exec{ 'install_rpm':
-    unless  => 'rpm -qa |grep irods-eudat-b2safe',
-    path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
-    command => "rpm -ivh /home/${::b2safe::irods::account_name}/rpmbuild/RPMS/noarch/irods-eudat-b2safe-${b2safe_package_version}.noarch.rpm",
-  } ->
-
-  file { '/opt/eudat/b2safe/packaging/install.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    content => template('b2safe/install.erb'),
   }
 
   if $run_scripts {
